c8d0c88ce493806b677532f6b2c628f184d08af6
ndegtyar <n.degtyar.kazanexpress@gmail.com>
2022-05-28 13:37:11 +0300

    add first auto test

---
+++ src/test/java/AutoTest.java
@@ -0,0 +1,22 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class AutoTest {
+    
+    @Test
+    public void openProfileTest() {
+        open("https://github.com");
+        $("[href='/login']").click();
+        $("[id='login_field']").sendKeys("n.degtyar.kazanexpress@gmail.com");
+        $("[id='password']").sendKeys("Degtyarn15");
+        $(".js-sign-in-button").click();
+        $(".Header").shouldBe(visible);
+        $("[aria-label='View profile and more']").click();
+        $(byText("Your profile")).click();
+        $(".vcard-names").shouldBe(visible);
+    }
+}
