10a8d5118975f8323fddea553052ea6483633bb4
ndegtyar <99661997+ndegtyar@users.noreply.github.com>
2022-06-05 15:53:58 +0300

    add some info from new lessons

--- src/test/java/AutoTest.java
+++ src/test/java/AutoTest.java
@@ -1,5 +1,6 @@
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
@@ -12,11 +13,14 @@ public class AutoTest {
         open("https://github.com");
         $("[href='/login']").click();
         $("[id='login_field']").sendKeys("n.degtyar.kazanexpress@gmail.com");
-        $("[id='password']").sendKeys("Degtyarn15");
+        $("[id='password']").sendKeys("sdgsgfs");
         $(".js-sign-in-button").click();
         $(".Header").shouldBe(visible);
         $("[aria-label='View profile and more']").click();
         $(byText("Your profile")).click();
         $(".vcard-names").shouldBe(visible);
     }
+
+    @ParameterizedTest()
+
 }
